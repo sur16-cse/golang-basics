cf81350de18a31eaf8eac01b7eed8a465bfe7344
Surbhi <surbhikumaridav@gmail.com>
2023-06-13 11:00:55 +0530

    urls, server

---
+++ 19urls/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string="https://lco.dev:3000/learn?coursename=reactjs&paymentid=ghbj456ghb"
+
+func main()  {
+	fmt.Println("Welcome to handling URLS in golang")
+	fmt.Println(myurl)
+	result,_:=url.Parse(myurl)
+	fmt.Println(result.Scheme)
+	fmt.Println(result.Host)
+	fmt.Println(result.Port())
+	fmt.Println(result.Path)
+	fmt.Println(result.RawQuery)
+
+	qparams:=result.Query()
+	fmt.Printf("The type of query params are: %T\n",qparams)
+	fmt.Println(qparams["coursename"])
+	for _,val:= range qparams{
+		fmt.Println("Param is:",val)
+	}
+
+	partsOfUrl:=&url.URL{
+		Scheme: "https",
+		Host: "lco.dev",
+		Path: "/tutcss",
+		RawPath: "user=hitesh",
+	}
+
+	anotherUrl:=partsOfUrl.String()
+	fmt.Println(anotherUrl)
+}
---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express, { json, urlencoded } from 'express'
+const app = express()
+const port =3000
+
+app.use(json())
+app.use(urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.status(200).send("Welcome to server")
+})
+
+app.get('/get',(req,res)=>{
+    res.status(200).json("Hello from surbhi")
+})
+
+app.post('/post',(req,res)=>{
+    let myJson=req.body
+    res.status(200).send(myJson)
+})
+
+app.post('/postform',(req,res)=>{
+    res.status(200).send(JSON.stringify(req.body))
+})
+
+app.listen(port,()=>{
+    console.log(`Example app listening at http://localhost:${port}`)
+})
